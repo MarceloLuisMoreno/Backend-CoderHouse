044ef47956229bab3761a7a1582d9d2dedfbae2a
Backend-CoderHouse <marceloluismoreno.gmail.com>
2022-07-13 16:08:20 -0300

    Ultimas actualizaciones.

--- trabajoFinalBackend/src/classes/CustomError.class.js
+++
@@ -1,9 +0,0 @@
-class CustomError {
-    constructor(codigo, descripcion, detalle) {
-        this.codigo = codigo;
-        this.descripcion = descripcion;
-        this.detalle = detalle;
-    }
-}
-
-module.exports = CustomError;
